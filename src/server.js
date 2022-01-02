c8c89328251b979a645923787ba1408688dbc5a0
Bomi Choi <hibomi97@gmail.com>
2022-01-02 21:06:30 +0900

    Course Completed

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import http from "http";
+import WebSocket from "ws";
+import SocketIO from "socket.io";
+// import { Server } from "socket.io";
+// import { instrument } from "@socket.io/admin-ui";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+// const wss = new WebSocket.Server({ httpServer });
+const wsServer = SocketIO(httpServer);
+// const wsServer = new Server(httpServer, {
+//     cors: {
+//         origin: ["https://admin.socket.io"],
+//         credentials: true
+//     }
+// });
+// instrument(wsServer, {
+//     auth: false
+// });
+
+wsServer.on("connection", (socket) => {
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    });
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
+    });
+});
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
